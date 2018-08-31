6615e0454262152b8b57d60923d6c1010b275675
sjay0010 <sjay0010@DESKTOP-P8KCJRL>
2018-09-01 03:44:27 +1000

    Add project files.

---
+++ FitnessBourneV2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FitnessBourneV2.MvcApplication" Language="C#" %>
---
+++ FitnessBourneV2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FitnessBourneV2.Startup))]
+namespace FitnessBourneV2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
