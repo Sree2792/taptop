7c31cf9fa60b28e0676ac98011628e26febe08c1
sjay0010 <sjay0010@502-B345-011WL>
2018-10-02 11:55:17 +1000

    Event Created or admin view development progress

---
+++ FitnessBourneV2/Controllers/EventsCreatedController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services;
+using FitnessBourneV2.Models;
+
+
+namespace FitnessBourneV2.Controllers
+{
+    public class EventsCreatedController : Controller
+    {
+        private fbmodelContainer db = new fbmodelContainer();
+
+        // GET: EventsCreated
+        public ActionResult EventCreated()
+        {
+            List<EventTable> eventList = db.EventTables.ToList();
+
+            List<EventTable> eventsJoined = new List<EventTable>();
+
+            //get login member table
+            MemberTable loginUser = new MemberTable();
+            foreach (MemberTable record in db.MemberTables.ToList())
+            {
+                if (record.Mem_Email_Id == User.Identity.Name)
+                {
+                    loginUser = record;
+                    break;
+                }
+            }
+
+            // Get members who joined the event
+            foreach (EventTable tableObj in eventList)
+            {
+                // Event member list
+                List<EventMembers> memObj = tableObj.EventMembers.ToList();
+
+                //get the members and check if login user there
+                foreach (EventMembers eveMemObj in memObj)
+                {
+                    if (eveMemObj.MemberTable.Mem_Id == loginUser.Mem_Id)
+                    {
+                        eventsJoined.Add(tableObj);
+                        break;
+                    }
+                }
+            }
+
+            // List of events to join
+            List<EventJoined> eventJoin = new List<EventJoined>();
+
+            // loop through joined events to make object
+            foreach (EventTable subRec in eventsJoined)
+            {
+                //setting record up
+                string navInstr = "";
+                string distanceStr = "";
+
+                string[] words = subRec.Evnt_NavigDetails.Split(';');
+
+                int counter = 0;
+
+                //Splitting navigation string to essential components
+                foreach (string content in words)
+                {
+                    if (counter == 0)
+                    {
+                        distanceStr = content;
+                    }
+                    else
+                    {
+                        if (counter == 1)
+                        {
+                            navInstr = content;
+                        }
+                        else
+                        {
+                            navInstr = navInstr + " -> " + content;
+                        }
+
+                    }
+                    counter = counter + 1;
+                }
+
+                //Setting location feeds
+                List<LocationTable> locList = subRec.LocationTables.ToList();
+                int startId = locList[0].Loc_Id;
+                int stopId = 0;
+                string startLoc = "";
+                string stopLoc = "";
+                List<string> checkPointList = new List<string>();
+                string checkPoint = "";
+                //getting location string
+                foreach (LocationTable record in locList)
+                {
+                    if (startId >= record.Loc_Id)
+                    {
+                        startLoc = record.Loc_Ref_Name;
+                        startId = record.Loc_Id;
+                    }
+                    if (stopId < record.Loc_Id)
+                    {
+                        stopLoc = record.Loc_Ref_Name;
+                        stopId = record.Loc_Id;
+                    }
+                    checkPointList.Add(record.Loc_Ref_Name);
+                }
+                //remove start and stop from check points
+                checkPointList.Remove(startLoc);
+                checkPointList.Remove(stopLoc);
+
+                //Setting up check point string
+                checkPoint = String.Join(" -> ", checkPointList.ToArray());
+
+                //setting up joined members list
+                string seatOccupied = "0";
+                if (subRec.EventMembers.Count > 0)
+                {
+                    // seat occupied
+                    seatOccupied = subRec.EventMembers.Count.ToString();
+                }
+
+                // Event type string set
+                string eventTypeStr = "";
+                foreach (EventType typeObj in db.EventTypes.ToList())
+                {
+                    if (typeObj.ET_Id == subRec.EventTypeET_Id)
+                    {
+                        eventTypeStr = typeObj.ET_Name;
+                    }
+                }
+
+                // event to pass to view
+                EventJoined eventToPass = new EventJoined()
+                {
+                    eventStartTime = subRec.Evnt_Start_DateTime.ToString(),
+                    eventEndTime = subRec.Evnt_End_DateTime.ToString(),
+                    totalCapacity = subRec.Evnt_Capacity.ToString(),
+                    navInstructions = navInstr,
+                    totalDistance = distanceStr,
+                    checkPoints = checkPoint,
+                    startLoc = startLoc,
+                    stopLoc = stopLoc,
+                    seatAvailblity = seatOccupied,
+                    eventID = subRec.Evnt_Id,
+                    EventypeInView = eventTypeStr
+                };
+
+                //Pass event
+                eventJoin.Add(eventToPass);
+            }
+
+            // session store
+            Session["JoinedEvents"] = eventJoin;
+
+            // event joined
+            EventJoinedModel eveJoinModel = new EventJoinedModel()
+            {
+                listOfEventJoined = eventJoin
+            };
+
+            // event joined
+            if (eventJoin.Count > 0)
+            {
+                return View(eveJoinModel);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
---
+++ FitnessBourneV2/Models/EventCreated.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FitnessBourneV2.Models
+{
+    public class EventCreated
+    {
+        public List<CreatedEventDetails> eventList { get; set; }
+    }
+
+    public class CreatedEventDetails
+    {
+        public int eventID { get; set; }
+        public string startLoc { get; set; }
+        public string stopLoc { get; set; }
+        public string checkPoints { get; set; }
+        public string totalDistance { get; set; }
+        public string navInstructions { get; set; }
+        public string eventStartTime { get; set; }
+        public string eventEndTime { get; set; }
+        public string seatAvailblity { get; set; }
+        public string totalCapacity { get; set; }
+        public string EventypeInView { get; set; }
+        public List<LocationTable> listOfLocations { get; set; }
+        public List<string> locationString { get; set; }
+        public List<MemberTable> EventMembers { get; set; }
+    }
+}
