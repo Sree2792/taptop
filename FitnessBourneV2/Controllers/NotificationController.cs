151572ee5f8763d0e2a69b2500f4bdca43312a2c
sjay0010 <sjay0010@502-B346-001WL>
2018-10-06 06:06:21 +1000

    Notification controller changes for confirm and delete actions

--- FitnessBourneV2/Controllers/NotificationController.cs
+++ FitnessBourneV2/Controllers/NotificationController.cs
@@ -91,7 +91,7 @@ namespace FitnessBourneV2.Controllers
                         stopDT = notifTable.EventEdit.EventTable.Evnt_End_DateTime.ToString(),
                         isEventAdmin = isAdmin,
                         youAre = userType,
-                        notifTbleId = actTable.NA_Id
+                        notifTbleId = notifTable.Notif_Id
                     };
 
                     //add object to list
@@ -125,16 +125,141 @@ namespace FitnessBourneV2.Controllers
             // get selected notificatio
             NotificationObject objNotif = (NotificationObject)Session["SelectedNotification"];
 
-            if(Session["NotificationStatus"].ToString() == "confirm")
-            {
-                //Confirm the edit of event and raise notification for all participants
+            //Confirm the edit of event and raise notification for all participants
+            NotificationTable notifObj = db.NotificationTables.Find(objNotif.notifTbleId);
 
+
+            // Event edit
+            EventEdit editObj = notifObj.EventEdit;
+
+            // Event table to update
+            EventTable eventTble = db.EventTables.Find(editObj.EE_EventIdToEdit);
+
+            //edited event
+            EventTable editedEventTble = editObj.EventTable;
+
+            if (Session["NotificationStatus"].ToString() == "confirm")
+            {
+                //Create new notifications for participant of events
+                if(eventTble.EventMembers.Count > 0)
+                {
+                    //there are members
+
+                    // get location details
+                    //add location
+                    List<String> locationList = (List<String>)Session["locationList"];
+
+                    List<LocationTable> localList = new List<LocationTable>();
+                    //Setting location feeds
+                    List<LocationTable> locList = eventTble.LocationTables.ToList();
+                    int startId = locList[0].Loc_Id;
+                    int stopId = 0;
+                    string startLoc = "";
+                    string stopLoc = "";
+                    List<string> checkPointList = new List<string>();
+                    string checkPoint = "";
+                    //getting location string
+                    foreach (LocationTable record in locList)
+                    {
+                        if (startId >= record.Loc_Id)
+                        {
+                            startLoc = record.Loc_Ref_Name;
+                            startId = record.Loc_Id;
+                        }
+                        if (stopId < record.Loc_Id)
+                        {
+                            stopLoc = record.Loc_Ref_Name;
+                            stopId = record.Loc_Id;
+                        }
+                        checkPointList.Add(record.Loc_Ref_Name);
+                    }
+                    //remove start and stop from check points
+                    checkPointList.Remove(startLoc);
+                    checkPointList.Remove(stopLoc);
+
+                    // Event type linking
+                    string eventType = "";
+                    foreach (var record in db.EventTypes.ToList())
+                    {
+                        if (record.ET_Id == eventTble.EventTypeET_Id)
+                        {
+                            eventType = record.ET_Name;
+                        }
+                    }
+
+                    // Participant notification
+                    NotificationTable notifTble = new NotificationTable()
+                    {
+                        Notif_Type = "p",
+                        Notif_Message = "The " + eventType + " event you registered on " + eventTble.Evnt_Start_DateTime.ToString() + " from " + startLoc + " to " + stopLoc
+                    };
+
+                    db.NotificationTables.Add(notifTble);
+                    db.SaveChanges();
+
+                    foreach(EventMembers eveMem in eventTble.EventMembers)
+                    {
+                        // loop through members of event
+                        // Notification table for notification
+                        NotificationActionTable actTble = new NotificationActionTable()
+                        {
+                            NA_Decision = "NO",
+                            MemberTable = eveMem.MemberTable,
+                            NotificationTable = notifTble
+                        };
+
+                        db.NotificationActionTables.Add(actTble);
+                        db.SaveChanges();
+                    }
+
+                    
+                }
+                
+                // edit event
+                editedEventTble.Evnt_Id = eventTble.Evnt_Id;
+
+                //confirm edit
+                db.Entry(editedEventTble).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+                //event edited delete
+                db.EventTables.Remove(editObj.EventTable);
+
+                //Eventedit delete
+                db.EventEdits.Remove(editObj);
+
+
+                //Notification delete
+                db.NotificationTables.Remove(notifObj);
+
+                //Delete Notification tables
+                foreach (NotificationActionTable tble in db.NotificationActionTables.ToList())
+                {
+                    if (tble.NotificationTable.Notif_Id == notifObj.Notif_Id)
+                    {
+                        db.NotificationActionTables.Remove(tble);
+                        db.SaveChanges();
+                    }
+                }
             }
             else if (Session["NotificationStatus"].ToString() == "delete")
             {
                 //Erase or delete the notification
+                //Notification delete
+                db.NotificationTables.Remove(notifObj);
+
+                //Delete Notification tables for the member as participant
+                foreach (NotificationActionTable tble in db.NotificationActionTables.ToList())
+                {
+                    if (tble.NotificationTable.Notif_Id == notifObj.Notif_Id && tble.MemberTable.Mem_Email_Id == User.Identity.Name)
+                    {
+                        db.NotificationActionTables.Remove(tble);
+                        db.SaveChanges();
+                        break;
+                    }
+                }
             }
-            return View();
+            return RedirectToAction("NotificationCenter", "Notification");
         }
 
         [WebMethod]
